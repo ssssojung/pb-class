87baa45c05c86cc3839babbcb76ed4380ac73e4c
ssssojung <1216thwjd@naver.com>
2023-03-30 03:01:09 +0900

    modal 완성

---
+++ week3-basic/js/script.js
@@ -0,0 +1,28 @@
+// tab
+
+//modal
+//로그인 눌렀을 때 모달
+const modal = document.getElementById("modal")
+const btnModal = document.getElementById("btn-modal")
+btnModal.addEventListener("click", e => {
+    modal.style.display = "flex"
+})
+//클로즈 버튼 
+const closeBtn = modal.querySelector(".close-area")
+closeBtn.addEventListener("click", e => {
+    modal.style.display = "none"
+})
+// 바깥 영역을 클릭
+modal.addEventListener("click", e => {
+    const evTarget = e.target
+    if(evTarget.classList.contains("modal-overlay")) {
+        modal.style.display = "none"
+    }
+})
+//esc버튼 
+window.addEventListener("keyup", e => {
+    if(modal.style.display === "flex" && e.key === "Escape") {
+        modal.style.display = "none"
+    }
+})
+//2depth header
