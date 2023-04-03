decb6832669b476c79037460755907996b00ee66
ssssojung <1216thwjd@naver.com>
2023-04-04 05:57:10 +0900

    스와이퍼 완료

--- week2-basic/js/script.js
+++ week2-basic/js/script.js
@@ -1,5 +1,15 @@
-var swiper = new Swiper(".mySwiper", {
+// var swiper = new Swiper(".mySwiper", {
+//     loop: true,
+//     pagination: {
+//       el: ".swiper-pagination",
+//     },
+//   });
+  var swiper = new Swiper(".mySwiper", {
     pagination: {
       el: ".swiper-pagination",
+      clickable: true,
+      renderBullet: function (index, className) {
+        return '<span class="' + className + '">'+ "</span>";
+      },
     },
   });
--- week3-basic/js/script.js
+++ week3-basic/js/script.js
@@ -1,45 +1,16 @@
-const tabs = document.querySelectorAll('[data-tab-target]')
-const tabContents = document.querySelectorAll('[data-tab-content]')
 
-tabs.forEach(tab => {
-  tab.addEventListener('mouseover', () => {
-    const target = document.querySelector(tab.dataset.tabTarget)
-    console.log(target);
-    tabContents.forEach(tabContent => {
-      tabContent.classList.remove('active')
-    });
-    tabs.forEach(tab => {
-      tab.classList.remove('active')
-    });
-    target.classList.add('active')
-    tab.classList.add('active')
-  })
-})
 
 
 //modal
-//로그인 눌렀을 때 모달
-const modal = document.getElementById("modal")
-const btnModal = document.getElementById("btn-modal")
-btnModal.addEventListener("click", e => {
-    modal.style.display = "flex"
-})
-//클로즈 버튼 
-const closeBtn = modal.querySelector(".close-area")
-closeBtn.addEventListener("click", e => {
-    modal.style.display = "none"
-})
-// 바깥 영역을 클릭
-modal.addEventListener("click", e => {
-    const evTarget = e.target
-    if(evTarget.classList.contains("modal-overlay")) {
-        modal.style.display = "none"
-    }
-})
-//esc버튼 
-window.addEventListener("keyup", e => {
-    if(modal.style.display === "flex" && e.key === "Escape") {
-        modal.style.display = "none"
-    }
-})
-//2depth header
+const modal = document.querySelector("#modal");
+const $btnModal = document.querySelector("#btn-modal");
+const $close = document.querySelector('#close-area');
+// con/st overlay = document.querySelector('')/
+window.addEventListener('click',(e)=>{
+  console.log(e.target)
+  if(e.target === $btnModal){
+    modal.classList.add('show');
+  }else if(e.target === modal || e.target === $close){
+    modal.classList.remove('show');
+  }
+});
