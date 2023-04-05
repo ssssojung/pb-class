d894b9e6a1b58606cd947f0067cbdb94812c48c7
ssssojung <1216thwjd@naver.com>
2023-04-05 16:52:28 +0900

    메뉴

--- week3-basic/js/script.js
+++ week3-basic/js/script.js
@@ -1,3 +1,34 @@
+const $tab1 = document.querySelectorAll('.tabs');
+const $tab2 = document.querySelectorAll('.tab');
+const tab2Con = document.querySelectorAll('.tab>ul');
+
+$tab1.forEach((tab, index) => {
+
+  tab.addEventListener('mouseover',() => {
+    $tab1[index].classList.add('show');
+    $tab2[index].classList.add('active');
+  })
+  tab.addEventListener('mouseout',()=>{
+    $tab1[index].classList.remove('show');
+    $tab2[index].classList.remove('active');
+
+  })
+});
+$tab2.forEach((tab, index) => {
+
+  tab.addEventListener('mouseover',() => {
+    $tab1[index].classList.add('show');
+    $tab2[index].classList.add('active');
+  })
+  tab.addEventListener('mouseout',()=>{
+    $tab1[index].classList.remove('show');
+    $tab2[index].classList.remove('active');
+
+  })
+});
+
+
+
 
 
 
@@ -5,7 +36,7 @@
 const modal = document.querySelector("#modal");
 const $btnModal = document.querySelector("#btn-modal");
 const $close = document.querySelector('#close-area');
-// con/st overlay = document.querySelector('')/
+
 window.addEventListener('click',(e)=>{
   console.log(e.target)
   if(e.target === $btnModal){
@@ -14,3 +45,5 @@ window.addEventListener('click',(e)=>{
     modal.classList.remove('show');
   }
 });
+
+//menu
