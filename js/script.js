58036e821ee15763b91ce03cda47748ab1cf18b4
ssssojung <1216thwjd@naver.com>
2023-04-20 06:28:02 +0900

    모바일 감지

--- js/script.js
+++ js/script.js
@@ -2,17 +2,33 @@
 (function(){
     const houseEle = document.querySelector('.house');
     const introEle = document.querySelector('.intro');
+    const worldEle = document.querySelector('.world');
+    const changeEle = document.querySelector('.change-notice');
     let maxScrollValue;
      
     const resizeHandler = () => {
         maxScrollValue = introEle.offsetHeight-window.innerHeight
+        if(window.innerWidth>window.innerHeight){
+            changeEle.style.display = 'none'
+        }else{
+            changeEle.style.display = 'flex'
+
+        }
     };
 
 
     window.addEventListener('scroll', ()=>{
         const zMove = scrollY/maxScrollValue *1000;
         houseEle.style.transform = `translateZ(${zMove}vw)`//스크롤바 트랙의 범위
+        console.log(scrollY);
+        if(scrollY > 1272){
+            worldEle.style.removeProperty = 'position';
+        }else{
+            worldEle.style.addProperty = 'fixed';
+        }
     }); 
+    
+    //스크롤 1272일때 world가 포지션 변경 되야함
 
     window.addEventListener('resize', resizeHandler);
     resizeHandler();
