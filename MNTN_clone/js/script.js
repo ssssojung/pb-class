3f58a3c2fa06560b5e2211ad6a4e25a443d812ed
ssssojung <1216thwjd@naver.com>
2023-04-20 09:14:47 +0900

    visual 섹션 애니메이션

--- MNTN_clone/js/script.js
+++ MNTN_clone/js/script.js
@@ -1,15 +1,15 @@
-function updateIndicator(entries, observer) {
-    const indicator = document.querySelector('#quick');
+// function updateIndicator(entries, observer) {
+//     const indicator = document.querySelector('#quick');
   
-    entries.forEach(entry => {
-      const index = entry.target.textContent.replace('#', '');
-      const el = indicator.querySelector(`[data-index="${index}"]`);    
-      el.classList.toggle('active', entry.isIntersecting);
-    });
-  }
+//     entries.forEach(entry => {
+//       const index = entry.target.textContent.replace('#', '');
+//       const el = indicator.querySelector(`[data-index="${index}"]`);    
+//       el.classList.toggle('active', entry.isIntersecting);
+//     });
+//   }
   
-  const io = new IntersectionObserver(updateIndicator);
+//   const io = new IntersectionObserver(updateIndicator);
   
-  Array.from(document.querySelectorAll('.block')).forEach(block => {
-    io.observe(block);
-  });
+//   Array.from(document.querySelectorAll('.block')).forEach(block => {
+//     io.observe(block);
+//   });
