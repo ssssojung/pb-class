c88a077a8ad4956384558bde5f4c0cbeeef6bdd4
ssssojung <1216thwjd@naver.com>
2023-05-07 22:05:23 +0900

    각 섹션 스크롤 높이 세팅

--- js/script.js
+++ js/script.js
@@ -4,11 +4,6 @@
     const introEle = document.querySelector('.intro');
     const worldEle = document.querySelector('.world');
     const changeEle = document.querySelector('.change-notice');
-    let maxScrollValue;
-    let yOffset = 0;
-    let prevScrollHeight = 0; 
-    let currentSection = 0;
-    let enterNewSection = false;
     
     const resizeHandler = () => {
         maxScrollValue = introEle.offsetHeight-window.innerHeight
@@ -28,75 +23,97 @@
             worldEle.style.position = 'fixed';
         }
     }); 
-    
-    // project
-    const sectionInfo = [
-        
-        {//intro
-            heightNum: 5,
-            scrollHeight: 0,
-        },
-        {//about
-            heightNum: 5,
-            scrollHeight: 0,
-            objs:{
-                container: document.querySelector('#scroll-section-1')
-            }
-        },
-        {//skill
-            heightNum: 5,
-            scrollHeight: 0,
-            objs:{
-                container: document.querySelector('#scroll-section-2')
-            }
-        },
-        {//project
-            type : 'sticky',
-            heightNum: 5,
-            scrollHeight: 0,
-            objs:{
-                container : document.querySelector('#scroll-section-3'),
-                projectA : document.querySelector('#scroll-section-3 .scroll-project-0'), 
-                projectB : document.querySelector('#scroll-section-3 .scroll-project-1'), 
-                projectC : document.querySelector('#scroll-section-3 .scroll-project-2'), 
-                projectD : document.querySelector('#scroll-section-3 .scroll-project-3') 
-            },
-            values:{
-                projectA_opacity : [0,1, {start: 0.1, end: 0.2}], 
-                projectB_opacity : [0,1, {start: 0.3, end: 0.4}], 
-                projectC_opacity : [0,1, {start: 0.5, end: 0.6}], 
-                projectD_opacity : [0,1, {start: 0.7, end: 0.8}] 
-            }
-        },
-        {//contact
-            heightNum: 5,
-            scrollHeight: 0,
-            objs:{
-                container: document.querySelector('#scroll-section-4')
-            }
-        },
-    ];
-
-    const setLayout = () => {
-        for(let i=0; i<sectionInfo.length; i++){
-            sectionInfo[i].scrollHeight = sectionInfo[i].heightNum*window.innerHeight;
-            sectionInfo[i].objs.container.style.height = `${sectionInfo[i].scrollHeight}px`;
-        }
-        console.log(yoffset,currentSection);
-        yOffset = window.scrollY;
-        let totalScrollHeight = 0;
-        for(let i=0; i<sectionInfo.length; i++){
-            totalScrollHeight += sectionInfo[i].scrollHeight;
-            if(totalScrollHeight>=yOffset){
-                currentSection = i;
-                break;
-            }
-        }
-    };
-
-     
 
     window.addEventListener('resize', resizeHandler);
     resizeHandler();
+    
+})();
 
+
+(()=>{
+    let yOffset = 0; // scrollY대신 쓸 변수
+    let prevScrollHeight = 0; // 현재 스크롤 위치보다 이전에 위치한 스크롤 높이값의 합
+    let currentScene = 0; // 현재 활성화된 섹션
+
+    const sceneInfo = [
+        { //0
+            type : 'normal',
+            heightNum : 2, //브라우저 높이의 5배로 scrollHeight 세팅
+            scrollHeight : 0,
+            objs: {
+                container: document.querySelector('#scroll-section-0')
+            }
+        },
+        { //1
+            type : 'sticky',
+            heightNum : 3.5,
+            scrollHeight : 0,
+            objs: {
+                container: document.querySelector('#scroll-section-1')
+            }
+        },
+        { //2
+            type : 'normal',
+            heightNum : 1,
+            scrollHeight : 0,
+            objs: {
+                container: document.querySelector('#scroll-section-2')
+            }
+            
+        },
+        { //3
+            type : 'sticky',
+            heightNum : 5,
+            scrollHeight : 0,
+            objs: {
+                container: document.querySelector('#scroll-section-3')
+            }
+        },
+        { //4
+            type : 'normal',
+            heightNum : 1,
+            scrollHeight : 0,
+            objs: {
+                container: document.querySelector('#scroll-section-4')
+            }
+        },
+        { //5
+            type : 'normal',
+            heightNum : 1,
+            scrollHeight : 0,
+            objs: {
+                container: document.querySelector('#scroll-section-5')
+            }
+        }
+    ];
+
+    const setLayout = () =>{
+        //각 스크롤 섹션의 높이 세팅
+        for(let i = 0; i < sceneInfo.length; i++){
+            sceneInfo[i].scrollHeight =  sceneInfo[i].heightNum * window.innerHeight ;
+            sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
+        }
+
+    }
+    const scrollLoop = () => {
+        prevScrollHeight = 0;
+        for(let i =0; i< currentScene; i++){
+            prevScrollHeight += sceneInfo[i].scrollHeight;
+        }
+        if(yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight){
+            currentScene++;
+        }
+        if(yOffset < prevScrollHeight){
+            currentScene--;
+        }
+        console.log(currentScene);
+    }
+
+    window.addEventListener('reszie', setLayout);
+    window.addEventListener('scroll',()=>{
+        yOffset = window.scrollY;
+        scrollLoop();
+    });
+    setLayout();
+ 
 })();
