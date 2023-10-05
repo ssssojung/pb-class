a3d97883067a661a3a4ca9c1090588d7331d02b1
ssssojung <1216thwjd@naver.com>
2023-10-05 19:07:46 +0900

    Sølve 메인페이지

---
+++ Solvl/js/script.js
@@ -0,0 +1,8 @@
+// const header = document.querySelector('header');
+
+// fetch('../component/header.html')
+// .then(res => res.text())
+// .then(data => header.innerHTML = data);
+// $(document).ready(function(){
+//     $('h2').css("border","1px solid red");
+// })
