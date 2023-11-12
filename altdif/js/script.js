4f34dce99febd0009d72f58b49b4f355341df544
ssssojung <1216thwjd@naver.com>
2023-11-12 23:33:36 +0900

    반응형

--- altdif/js/script.js
+++ altdif/js/script.js
@@ -132,8 +132,11 @@ const swiper = new Swiper('.swiper', {
     keyboard: {
         enabled: true,
       },
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
   
-    // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
