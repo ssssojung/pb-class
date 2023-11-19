b0b690a1f770d9ca23080e09b637587b298dbbe1
ssssojung <1216thwjd@naver.com>
2023-11-20 03:30:34 +0900

    layout_animation추가

--- eleganttable/js/script.js
+++ eleganttable/js/script.js
@@ -8,6 +8,7 @@ const $next = document.querySelector('.slide_btn .next');
 const btnline = document.querySelector('.slide_btn span');
 
 
+//btn
 Bg.addEventListener('click',(e)=>{
     console.log(i)
     e.preventDefault;
@@ -17,18 +18,50 @@ Bg.addEventListener('click',(e)=>{
     if(e.target === $prev){
         slidePrevHandler();
     }
-    if(i==1){
-        $prev.style.color = '#131313';
-        $next.style.color = '#131313';
-        btnline.style.backgroundColor = '#131313';
-    }else{
-        $prev.style.color = '#fff';
-        $next.style.color = '#fff';
-        btnline.style.backgroundColor = '#fff';
-    }
-    $prev.innerText = `0${i+1}`;
-    $next.innerText = `0${i+2}`;
+
 });
+
+//wheel
+window.addEventListener('wheel',(e)=>{
+    console.log(e.deltaY)
+    if(e.deltaY > 0){
+        console.log('다음')
+
+    }else if(e.deltaY < 0){
+        console.log('이전')
+    }
+})
+
+//auto
+function autoPlay(){
+    slideNextHandler();
+}
+
+let interval = setInterval(autoPlay, 5000);
+let autotoggle = true;
+
+const slide_section = document.querySelectorAll('.slide_section');
+const slide_btn = document.querySelector('.slide_btn');
+const slide_right = document.querySelectorAll('.slide_right');
+
+for(let i=0; i<slide_section.length; i++){
+    slide_section[i].querySelector('.banner-img').addEventListener('click',function(e){
+        slide_section[i].querySelector('.contentBox').classList.add('open');
+        slide_section[i].querySelector('.open-layout').classList.add('open');
+        slide_btn.classList.add('fade-out-bottom');
+
+        //autoPlay
+        if(autotoggle){
+            clearInterval(interval);
+            autotoggle = false;
+        }else{
+            interval = setInterval(text, 500);
+            autotoggle = true;
+        }
+
+    })
+
+}
 
 const slideNextHandler = () =>{
     slideList[i].classList.remove('active');
@@ -40,6 +73,18 @@ const slideNextHandler = () =>{
     slideList[i].classList.add('active');
     Bg.style.backgroundColor = ColorArr[i];
 
+    if(i==1){
+    $prev.style.color = '#131313';
+    $next.style.color = '#131313';
+    btnline.style.backgroundColor = '#131313';
+    }else{
+        $prev.style.color = '#fff';
+        $next.style.color = '#fff';
+        btnline.style.backgroundColor = '#fff';
+    }
+    $prev.innerText = `0${i+1}`;
+    $next.innerText = `0${i+2}`;
+
 }
 const slidePrevHandler = () =>{
     slideList[i].classList.remove('active');
@@ -50,28 +95,41 @@ const slidePrevHandler = () =>{
     }
     slideList[i].classList.add('active');
     Bg.style.backgroundColor = ColorArr[i];
+
+    if(i==1){
+        $prev.style.color = '#131313';
+        $next.style.color = '#131313';
+        btnline.style.backgroundColor = '#131313';
+        }else{
+            $prev.style.color = '#fff';
+            $next.style.color = '#fff';
+            btnline.style.backgroundColor = '#fff';
+        }
+        $prev.innerText = `0${i+1}`;
+        $next.innerText = `0${i+2}`;
 }
 
-const slide_section = document.querySelectorAll('.slide_section');
-const slide_btn = document.querySelector('.slide_btn');
-const slide_right = document.querySelectorAll('.slide_right');
 
-for(let i=0; i<slide_section.length; i++){
-    slide_section[i].querySelector('.banner-img').addEventListener('click',function(e){
-        slide_section[i].querySelector('.contentBox').classList.add('open');
-        // slide_right.classList.add('open');
-        slide_btn.classList.add('fade-out-bottom');
-        // slide_btn.classList.add('fade-out-bottom');
-        // slide_right.border ="1px solid red";
-        // setTimeout(function() {
-        //     console.log('dispay:none');
-        //     slide_section[i].querySelector('.open span').classList.add('D-none');
-        //   }, 1000);
-    })
+// if(chk) {
+//     // 휠 일정시간동안 막기
+//     chk = false;
+//     setTimeout(function(){
+//         chk = true;
+//     }, 500)
 
-}
-
-// const moreViewHandler = () =>{
-
+//     // 휠 방향 감지(아래: -120, 위: 120)
+//     let w_delta = event.wheelDelta / 120;
+    
+//     // 휠 아래로
+//     if(w_delta < 0 && $(this).next().length > 0) {
+//         $('.container').animate({
+//             left: -array[$(this).index()+1]
+//         }, 500)
+//     }
+//     // 휠 위로
+//     else if(w_delta > 0 && $(this).prev().length > 0) {
+//         $('.container').animate({
+//             left: -array[$(this).index()-1]
+//         }, 500)
+//     }
 // }
-
